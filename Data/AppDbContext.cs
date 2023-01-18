dc3ab647c8f90d6b530adf2ec83d49825aa2a387
HDragos24 <hc.dragos@gmail.com>
2023-01-18 18:17:29 +0200

    Add project files.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebProject.Model;
+
+namespace WebProject.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Category> Category { get; set; }
+    }
+}
---
+++ Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebProject.Data;
+using WebProject.Model;
+
+namespace WebProject.Pages.Categories
+{
+    public class IndexModel : PageModel
+    {
+        private readonly AppDbContext _db;
+        public IEnumerable<Category> Categories { get; set; }
+
+        public IndexModel(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public void OnGet()
+        {
+            Categories = _db.Category;
+        }
+    }
+}
