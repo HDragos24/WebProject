e5da63527eaaf53c638ecfb9cb47d3fea6a95e17
HDragos24 <117278503+HDragos24@users.noreply.github.com>
2023-01-22 12:29:39 +0200

    Add files via upload

---
+++ WebProject.DataAccess/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebProject.Models;
+
+namespace WebProject.DataAccess.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Category> Category { get; set; }
+        public DbSet<FoodType> FoodType { get; set; }
+        public DbSet<MenuItem> MenuItem { get; set; }
+    }
+}
