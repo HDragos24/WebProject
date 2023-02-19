6472359d3de981f80583d6d4d65b8687ea72f1b7
HDragos24 <117278503+HDragos24@users.noreply.github.com>
2023-02-19 17:39:03 +0200

    Add files via upload

---
+++ FoodType.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebProject.Models
+{
+    public class FoodType
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
